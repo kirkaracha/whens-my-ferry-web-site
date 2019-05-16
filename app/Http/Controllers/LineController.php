c7fcc160ec7d85cdd8eaa8511a23e84f865c982f
Kirk Franklin <kirk@magneato.com>
2019-05-16 16:51:00 -0700

    Set up controllers

---
+++ app/Http/Controllers/LineController.php
@@ -0,0 +1,87 @@
+<?php
+
+declare(strict_types=1);
+
+namespace WhensMyFerry\Http\Controllers;
+
+use WhensMyFerry\Data\Lines\Line;
+use Illuminate\Http\Request;
+
+class LineController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \WhensMyFerry\Line  $line
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Line $line)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \WhensMyFerry\Line  $line
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Line $line)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \WhensMyFerry\Line  $line
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Line $line)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \WhensMyFerry\Line  $line
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Line $line)
+    {
+        //
+    }
+}
---
+++ app/Http/Controllers/PatternController.php
@@ -0,0 +1,87 @@
+<?php
+
+declare(strict_types=1);
+
+namespace WhensMyFerry\Http\Controllers;
+
+use WhensMyFerry\Data\Patterns\Pattern;
+use Illuminate\Http\Request;
+
+class PatternController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \WhensMyFerry\Pattern  $pattern
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Pattern $pattern)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \WhensMyFerry\Pattern  $pattern
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Pattern $pattern)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \WhensMyFerry\Pattern  $pattern
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Pattern $pattern)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \WhensMyFerry\Pattern  $pattern
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Pattern $pattern)
+    {
+        //
+    }
+}
